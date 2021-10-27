cd712990eef7f19fbaf217d1a24794f5885dc490
Nate Nasteff <nnasteff@protonmail.com>
2021-10-26 23:56:07 -0400

    added table number tests

--- src/test/Mag/MagConstructorTest.ts
+++ src/test/Mag/MagConstructorTest.ts
@@ -1,32 +1,37 @@
 import { Mag } from "../../assets/Mag";
 import { expect } from "chai";
 
-const mag = new Mag("Mag");
-const mag2 = new Mag("Kalki");
-const mag3 = new Mag("Vayu");
-const mag4 = new Mag("Kama");
+const mags: Mag[] = [
+  new Mag("Mag"), new Mag("Kalki"), new Mag("Vayu"),
+  new Mag("Naga"), new Mag("Savitri")
+];
 
-// Testing mag levels
+// Testing mag base levels
 
 describe('Mag level test', () => {
   
   it('check the level of a base mag', () => {
-    expect(mag.magLevel).to.equal(5);
+    expect(mags[0].magLevel).to.equal(5);
 
   });
 
   it('check the level of a 1st evolution mag', () => {
-    expect(mag2.magLevel).to.equal(10);
+    expect(mags[1].magLevel).to.equal(10);
 
   });
 
   it('check the level of an 2nd evolution mag', () => {
-    expect(mag3.magLevel).to.equal(35);
+    expect(mags[2].magLevel).to.equal(35);
 
   });
 
   it('check the level of an 3rd evolution mag', () => {
-    expect(mag4.magLevel).to.equal(50);
+    expect(mags[3].magLevel).to.equal(50);
+
+  });
+
+  it('check the level of an 3rd evolution mag', () => {
+    expect(mags[4].magLevel).to.equal(100);
 
   });
 
@@ -37,13 +42,85 @@ describe('Mag level test', () => {
 describe('Mag name test', () => {
 
   it('check if base mag name is correct', () => {
-    expect(mag.magName).to.equal("Mag");
+    expect(mags[0].magName).to.equal("Mag");
 
   });
 
   it('check the name of a 1st evolution mag', () => {
-    expect(mag2.magName).to.equal("Kalki");
+    expect(mags[1].magName).to.equal("Kalki");
 
   });
 
+  it('check the name of a 2nd evolution mag', () => {
+    expect(mags[2].magName).to.equal("Vayu");
+
+  });
+
+  it('check the name of a 3rd evolution mag', () => {
+    expect(mags[3].magName).to.equal("Naga");
+
+  });
+
+  it('check the name of a 1st evolution mag', () => {
+    expect(mags[4].magName).to.equal("Savitri");
+
+  });
 })
+  
+describe('Mag level test', () => {
+  
+  it('check the level of a base mag', () => {
+    expect(mags[0].magLevel).to.equal(5);
+
+  });
+
+  it('check the level of a 1st evolution mag', () => {
+    expect(mags[1].magLevel).to.equal(10);
+
+  });
+
+  it('check the level of an 2nd evolution mag', () => {
+    expect(mags[2].magLevel).to.equal(35);
+
+  });
+
+  it('check the level of an 3rd evolution mag', () => {
+    expect(mags[3].magLevel).to.equal(50);
+
+  });
+
+  it('check the level of an 4th evolution mag', () => {
+    expect(mags[4].magLevel).to.equal(100);
+
+  });
+});
+
+// Testing mag names
+
+describe('Mag feed table number test', () => {
+
+  it('confirm feed table number for base mag', () => {
+    expect(mags[0].feedChart.tableNum).to.equal(0);
+
+  });
+
+  it('confirm feed table number for 1st evolution mag', () => {
+    expect(mags[1].feedChart.tableNum).to.equal(1);
+
+  });
+
+  it('confirm feed table number for 2nd evolution mag', () => {
+    expect(mags[2].feedChart.tableNum).to.equal(4);
+
+  });
+
+  it('confirm feed table number for 3rd evolution mag', () => {
+    expect(mags[3].feedChart.tableNum).to.equal(6);
+
+  });
+
+  it('confirm feed table number for 4th evolution mag', () => {
+    expect(mags[4].feedChart.tableNum).to.equal(7);
+
+  });
+}); 
